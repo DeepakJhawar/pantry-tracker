ebb56c4d9510f5fbc68c08e20475255bc7ffc22c
Deepak Jhawar <deepakjhawar04@gmail.com>
2024-08-02 11:17:20 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,389 @@
-import Image from "next/image";
+"use client";
+
+import {
+  Box,
+  Stack,
+  Typography,
+  Button,
+  Modal,
+  TextField,
+  IconButton,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { useState, useEffect } from "react";
+import { firestore } from "@/firebase";
+import {
+  collection,
+  doc,
+  query,
+  getDocs,
+  setDoc,
+  deleteDoc,
+  getDoc,
+} from "firebase/firestore";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "90%",
+  maxWidth: "400px",
+  bgcolor: "white",
+  boxShadow: 24,
+  p: 4,
+  display: "flex",
+  flexDirection: "column",
+  gap: 2,
+  backgroundColor: "rgba(255, 255, 255, 0.8)",
+  borderRadius: 5,
+};
 
 export default function Home() {
+  const [pantry, setPantry] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const [alert, setAlert] = useState(null);
+
+  const [openAdd, setOpenAdd] = useState(false);
+  const [openSearch, setOpenSearch] = useState(false);
+
+  const handleOpenAdd = () => setOpenAdd(true);
+  const handleCloseAdd = () => setOpenAdd(false);
+
+  const handleOpenSearch = () => setOpenSearch(true);
+  const handleCloseSearch = () => setOpenSearch(false);
+
+  const [itemName, setItemName] = useState("");
+
+  const updatePantry = async () => {
+    const snapshot = query(collection(firestore, "inventory"));
+    const docs = await getDocs(snapshot);
+    const pantryList = [];
+    docs.forEach((doc) => {
+      pantryList.push({ name: doc.id, ...doc.data() });
+    });
+    setPantry(pantryList);
+    // Also clear search results if pantry is updated
+    setSearchResults([]);
+  };
+
+  useEffect(() => {
+    updatePantry();
+  }, []);
+
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const { count } = docSnap.data();
+      await setDoc(docRef, { count: count + 1 });
+    } else {
+      await setDoc(docRef, { count: 1 });
+    }
+    await updatePantry();
+  };
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(docRef);
+    await deleteDoc(docRef);
+    await updatePantry();
+  };
+
+  const search = async (item) => {
+    if (!item.trim()) {
+      // Reset search results if input is empty
+      setSearchResults([]);
+      return;
+    }
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      setSearchResults([{ name: item, ...docSnap.data() }]);
+    } else {
+      setSearchResults([]);
+    }
+  };
+
+  const reduceItem = async (item) => {
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const { count } = docSnap.data();
+      if (count === 1) {
+        await deleteDoc(docRef);
+      } else {
+        await setDoc(docRef, { count: count - 1 });
+      }
+    }
+    await updatePantry();
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      gap={2}
+      sx={{
+        backgroundImage: `url(/bg.jpg)`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        p: 2,
+        overflow: "hidden",
+      }}
+    >
+      <Modal
+        open={openAdd}
+        onClose={handleCloseAdd}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            fontFamily={"Quicksand Variable"}
+            textAlign="center"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            Add Item
+          </Typography>
+          <Stack width="100%" spacing={2}>
+            <TextField
+              id="outlined-basic"
+              label="Item"
+              variant="outlined"
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
             />
-          </a>
-        </div>
-      </div>
+            <Button
+              variant="contained"
+              onClick={() => {
+                addItem(itemName);
+                setItemName("");
+                handleCloseAdd();
+              }}
+              sx={{
+                backgroundColor: "rgba(193,177,160, 0.5)",
+                color: "rgba(40,40,40, 0.9)",
+                "&:hover": {
+                  backgroundColor: "rgba(89,70,48, 0.6)",
+                  color: "white",
+                },
+              }}
+            >
+              Add
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Modal
+        open={openSearch}
+        onClose={handleCloseSearch}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            fontFamily={"Quicksand Variable"}
+            textAlign="center"
+          >
+            Search Item
+          </Typography>
+          <Stack width="100%" spacing={2}>
+            <TextField
+              id="outlined-basic"
+              label="Item"
+              variant="outlined"
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
+            />
+            <Button
+              variant="contained"
+              onClick={() => {
+                search(itemName);
+                setItemName("");
+                handleCloseSearch();
+              }}
+              sx={{
+                backgroundColor: "rgba(193,177,160, 0.5)",
+                color: "rgba(40,40,40, 0.9)",
+                "&:hover": {
+                  backgroundColor: "rgba(89,70,48, 0.6)",
+                  color: "white",
+                },
+              }}
+            >
+              Search
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        spacing={2}
+        mb={2}
+        sx={{
+          width: "100%",
+          maxWidth: "500px",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Button
+          variant="contained"
+          onClick={handleOpenAdd}
+          sx={{
+            backgroundColor: "rgba(193,177,160, 0.5)",
+            "&:hover": {
+              backgroundColor: "rgba(89,70,48, 0.6)",
+              color: "white",
+            },
+            width: "100%",
+            maxWidth: "240px",
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
+          Add
+        </Button>
+        <Button
+          variant="contained"
+          onClick={handleOpenSearch}
+          sx={{
+            backgroundColor: "rgba(193,177,160, 0.5)",
+            "&:hover": {
+              backgroundColor: "rgba(89,70,48, 0.6)",
+              color: "white",
+            },
+            width: "100%",
+            maxWidth: "240px",
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          Search
+        </Button>
+      </Stack>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Box
+        width="100%"
+        maxWidth="600px"
+        height="auto"
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        borderRadius={4}
+        marginBottom={2}
+        sx={{
+          backgroundColor: "rgba(193,177,160, 0.5)",
+        }}
+      >
+        <Typography
+          variant={"h2"}
+          color={"#333"}
+          textAlign={"center"}
+          fontFamily={"Quicksand Variable"}
+          paddingY={2}
+          fontSize={{ xs: "h4.fontSize", sm: "h2.fontSize" }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          Pantry Items
+        </Typography>
+      </Box>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Box
+        width="100%"
+        maxWidth="600px"
+        height="300px"
+        sx={{
+          overflowX: "auto",
+          overflowY: "auto",
+          px: 2,
+          "&::-webkit-scrollbar": {
+            width: "8px",
+          },
+          "&::-webkit-scrollbar-track": {
+            background: "#f1f1f1",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            background: "#888",
+            borderRadius: "8px",
+          },
+          "&::-webkit-scrollbar-thumb:hover": {
+            background: "#555",
+          },
+        }}
+      >
+        <Stack spacing={2}>
+          {(searchResults.length > 0 ? searchResults : pantry).map(
+            ({ name, count }) => (
+              <Box
+                key={name}
+                width="100%"
+                minHeight="70px"
+                display={"flex"}
+                flexDirection={{ xs: "column", sm: "row" }}
+                justifyContent={"space-between"}
+                alignItems={"center"}
+                bgcolor={"#f0f0f0"}
+                borderRadius="16px"
+                paddingX={2}
+                paddingY={1}
+                sx={{
+                  backgroundColor: "rgba(255,255,255, 0.7)",
+                }}
+              >
+                <Stack
+                  spacing={1}
+                  width="100%"
+                  textAlign={{ xs: "center", sm: "left" }}
+                >
+                  <Typography
+                    variant={"h4"}
+                    color={"#333"}
+                    fontFamily={"Quicksand Variable"}
+                    fontSize={{ xs: "h6.fontSize", sm: "h4.fontSize" }}
+                  >
+                    {name.charAt(0).toUpperCase() + name.slice(1)}
+                  </Typography>
+                  <Typography
+                    variant={"h5"}
+                    color={"#333"}
+                    fontFamily={"Quicksand Variable"}
+                    fontSize={{ xs: "body1.fontSize", sm: "h5.fontSize" }}
+                  >
+                    Available: {count}
+                  </Typography>
+                </Stack>
+                <Stack direction="row" spacing={1}>
+                  <IconButton aria-label="add" onClick={() => addItem(name)}>
+                    <AddIcon fontSize="small" />
+                  </IconButton>
+                  <IconButton
+                    aria-label="remove"
+                    onClick={() => reduceItem(name)}
+                  >
+                    <RemoveIcon fontSize="small" />
+                  </IconButton>
+                  <IconButton
+                    aria-label="delete"
+                    onClick={() => removeItem(name)}
+                  >
+                    <DeleteIcon fontSize="small" />
+                  </IconButton>
+                </Stack>
+              </Box>
+            )
+          )}
+        </Stack>
+      </Box>
+    </Box>
   );
 }
